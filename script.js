85d940c87bc0f3d67a816b6246d5fe78123796f0
djavlarwav <DjavlarProductions@gmail.com>
2026-06-05 16:34:11 -0700

    Update script.js

--- script.js
+++ script.js
@@ -1,12 +1,38 @@
 function showPage(pageId){
 
     const pages = document.querySelectorAll('.page');
+    const buttons = document.querySelectorAll('nav button');
 
     pages.forEach(page=>{
         page.classList.remove('active');
     });
 
+    buttons.forEach(btn=>{
+        btn.classList.remove('active');
+    });
+
     document
         .getElementById(pageId)
         .classList.add('active');
+
+    const activeButton = [...buttons].find(
+        btn =>
+        btn.textContent.toLowerCase() === pageId
+    );
+
+    if(activeButton){
+        activeButton.classList.add('active');
+    }
+
 }
+
+document.addEventListener('DOMContentLoaded',()=>{
+
+    const homeButton = [...document.querySelectorAll('nav button')]
+        .find(btn=>btn.textContent==="HOME");
+
+    if(homeButton){
+        homeButton.classList.add('active');
+    }
+
+});
