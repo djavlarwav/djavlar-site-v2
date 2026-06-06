ffa03b638cc87c5663e67d8ae51293a97200fa0c
djavlarwav <DjavlarProductions@gmail.com>
2026-06-05 23:06:10 -0700

    Update script.js

--- script.js
+++ script.js
@@ -1,39 +1,49 @@
-function showPage(pageId){
+function showPage(pageId, button){
 
-    document.querySelectorAll('.page').forEach(page=>{
-        page.classList.remove('active');
+    // Hide all pages
+    document.querySelectorAll(".page").forEach(page=>{
+        page.classList.remove("active");
     });
 
-    document.querySelectorAll('nav button').forEach(button=>{
-        button.classList.remove('active');
+    // Remove active nav underline
+    document.querySelectorAll(".nav-btn").forEach(btn=>{
+        btn.classList.remove("active");
     });
 
-    document.getElementById(pageId).classList.add('active');
+    // Show selected page
+    document
+        .getElementById(pageId)
+        .classList
+        .add("active");
 
-    const activeButton = document.querySelector(
-        `nav button[onclick*="${pageId}"]`
-    );
-
-    if(activeButton){
-        activeButton.classList.add('active');
+    // Underline selected nav button
+    if(button){
+        button.classList.add("active");
     }
 
+    // Return to top
     window.scrollTo({
         top:0,
-        behavior:'smooth'
+        behavior:"smooth"
     });
+
 }
 
-document.addEventListener('DOMContentLoaded',()=>{
 
-    document.getElementById('home').classList.add('active');
+// Make HOME active when website loads
 
-    const homeBtn = document.querySelector(
-        `nav button[onclick*="home"]`
-    );
+document.addEventListener("DOMContentLoaded",()=>{
 
-    if(homeBtn){
-        homeBtn.classList.add('active');
+    document
+        .getElementById("home")
+        .classList
+        .add("active");
+
+    const homeButton =
+    document.querySelector(".nav-btn");
+
+    if(homeButton){
+        homeButton.classList.add("active");
     }
 
 });
