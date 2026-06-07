fcf4d6aeea82cdce72010a3834dc052467a8e513
djavlarwav <DjavlarProductions@gmail.com>
2026-06-06 17:50:39 -0700

    Update script.js

--- script.js
+++ script.js
@@ -1,28 +1,29 @@
 function showPage(pageId, button){
 
-  // Hide all pages
-document.querySelectorAll(".page").forEach(page=>{
-    page.classList.remove("active");
-});
+    // Hide all pages
+    document.querySelectorAll(".page").forEach(page=>{
+        page.classList.remove("active");
+    });
 
-// Remove active nav underline
-document.querySelectorAll(".nav-btn").forEach(btn=>{
-    btn.classList.remove("active");
-});
+    // Remove active nav underline
+    document.querySelectorAll(".nav-btn").forEach(btn=>{
+        btn.classList.remove("active");
+    });
 
-// Show selected page
-document
-    .getElementById(pageId)
-    .classList
-    .add("active");
+    // Show selected page
+    document
+        .getElementById(pageId)
+        .classList
+        .add("active");
 
-// Activate nav button
-if(button){
-    button.classList.add("active");
+    // Activate nav button
+    if(button){
+        button.classList.add("active");
+    }
+
+    // Instantly return to top
+    window.scrollTo(0,0);
 }
-
-// Instantly return to top
-window.scrollTo(0,0);
 
 
 // Make HOME active when website loads
